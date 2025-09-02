6d2d67cad3a4a3a99850d2d1910310cfc96c6002
Pravallikaa0 <pravallikagannavaram03@gmail.com>
2025-09-02 18:11:28 +0530

    Add files via upload

---
+++ ravi.java
@@ -0,0 +1,16 @@
+public class ravi {
+    public static void main(String [] args){
+int age = 16;
+Boolean haspassport = false;
+Boolean hasnocriminalRecord = true;
+if(age >= 18 && haspassport == true && hasnocriminalRecord == true){
+System.out.println("elligible for visa");
+}
+else if(age >= 18 && haspassport == false && hasnocriminalRecord == true){
+System.out.println("apply for passport first");
+}
+else{
+System.out.println("not eligible");
+}
+}
+}
